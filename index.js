96245f0742617ee184cd944c6be0e71d833be193
Your Name <real@email.com>
2023-02-16 11:56:45 -0500

    Created Folder and .js and .html files

---
+++ index.js
@@ -0,0 +1,14 @@
+// Phase 2: JS
+// Create index.js and link to it in your HTML file.
+
+// Your code should do the following:
+// - Maintain an array of objects representing all of the user's added bookmarks.
+// - Re-render the entire list of bookmarks any time a bookmark is added or removed.
+// - Use map to map the bookmark array to DOM elements.
+
+// Here are some guiding questions:
+//     1. What event listeners do you need to start off with?
+//     2. Can you correctly add bookmarks?
+//     3. What UI element allows a user to remove a previously added bookmark?
+//     4. How can you make sure that the correct bookmark is removed?
+
